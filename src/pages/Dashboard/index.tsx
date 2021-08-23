2efb7be1c69c0182834ace4a5af3b5eff1007e7d
Welisson luca <welissonluca17@gmail.com>
2021-08-23 09:47:26 -0300

    feat: finihs dashboard page

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,53 @@
 import React from 'react';
-import { Title } from './style';
+import { FiChevronRight } from 'react-icons/fi';
+
+import logoImg from '../../assets/logo.svg';
+import { Form, Repositories, Title } from './style';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore repositórios no github</Title>;
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore repositórios no github</Title>
+
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+      <Repositories>
+        <a href="teste">
+          <img src="https://avatars.githubusercontent.com/u/62263143?v=4" alt="Welisson" />
+
+          <div>
+            <strong>goBarber - api</strong>
+            <p>Gostack repository</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img src="https://avatars.githubusercontent.com/u/62263143?v=4" alt="Welisson" />
+
+          <div>
+            <strong>goBarber - api</strong>
+            <p>Gostack repository</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img src="https://avatars.githubusercontent.com/u/62263143?v=4" alt="Welisson" />
+
+          <div>
+            <strong>goBarber - api</strong>
+            <p>Gostack repository</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
