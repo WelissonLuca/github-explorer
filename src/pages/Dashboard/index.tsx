a2a922a0a8618ea6211725d077c1662175fb020e
Welisson luca <welissonluca17@gmail.com>
2021-08-23 23:46:58 -0300

    feat: add page repositories

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,5 +1,6 @@
 import React, { useState, FormEvent, useEffect } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
 
 import logoImg from '../../assets/logo.svg';
 import api from '../../services/api';
@@ -58,7 +59,7 @@ const Dashboard: React.FC = () => {
       {inputError && <Error>{inputError}</Error>}
       <Repositories>
         {repositories.map((repository) => (
-          <a key={repository.full_name} href="teste">
+          <Link key={repository.full_name} to={`/repositories/${repository.full_name}`}>
             <img src={repository.owner.avatar_url} alt={repository.owner.login} />
 
             <div>
@@ -67,7 +68,7 @@ const Dashboard: React.FC = () => {
             </div>
 
             <FiChevronRight size={20} />
-          </a>
+          </Link>
         ))}
       </Repositories>
     </>
--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,7 +1,62 @@
 import React from 'react';
+import { FiChevronRight, FiChevronsLeft } from 'react-icons/fi';
+import { useRouteMatch, Link } from 'react-router-dom';
 
+import logoImg from '../../assets/logo.svg';
+
+import { Header, RepositoryInfo, Issues } from './style';
+
+interface RepositoryParams {
+  repository: string;
+}
 const Repository: React.FC = () => {
-  return <h1>Repository</h1>;
+  const { params } = useRouteMatch<RepositoryParams>();
+  return (
+    <>
+      <Header>
+        <img src={logoImg} alt="Github Explorer" />
+        <Link to="/dashboard">
+          <FiChevronsLeft size={16} />
+          Voltar
+        </Link>
+      </Header>
+      <RepositoryInfo>
+        <header>
+          <img src="https://avatars.githubusercontent.com/u/62263143?v=4" alt="Welisson" />
+          <div>
+            <strong>Welisson/WelissonLuca</strong>
+            <p>Readme para meu perfil</p>
+          </div>
+        </header>
+
+        <ul>
+          <li>
+            <strong>818</strong>
+            <span>Stars</span>
+          </li>
+          <li>
+            <strong>818</strong>
+            <span>Forks</span>
+          </li>
+          <li>
+            <strong>818</strong>
+            <span>Issues Abertas</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+
+      <Issues>
+        <Link to="/repositories/">
+          <div>
+            <strong>aaaaaaaa</strong>
+            <p>aa</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </Link>
+      </Issues>
+    </>
+  );
 };
 
 export default Repository;
